f66cda41b337bf4f88c6906d1c803432608f9d3c
EliasMez <elias.mezine@laposte.net>
2023-02-03 10:23:36 +0100

    dockers ok render API ok

--- django/loanProject/views.py
+++ django/loanProject/views.py
@@ -4,15 +4,15 @@ from json import dumps, loads
 import requests
  
 local_path = 'http://0.0.0.0:8001/predict'
-# render_path = 
+render_path = 'https://api-sba.onrender.com/predict'
 
 def homePageView(request):
     responseForm = forms.PredictionForm(request.POST or None)
     if request.method =="POST" :
         if responseForm.is_valid() :
             data = dumps(responseForm.cleaned_data)
-            response = requests.post(local_path, data=data)
-            context = {'response' : response.text}
+            response = requests.post(render_path, data=data)
+            context = {'response' : response.json()['MIS_Status']}
             return render(request, 'forecast.html', context=context)
     context = {'form' : responseForm}
     return render(request, 'home.html', context=context)
